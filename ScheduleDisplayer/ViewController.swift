f3d9d80535f3336eae6d2b979a16c7ee3058f904
Fiel Faustino <john.faustino@obf.ateneo.edu>
2018-07-12 18:49:11 +0800

    Added JSON retrieval functionality for TV show schedules

--- ScheduleDisplayer/ViewController.swift
+++ ScheduleDisplayer/ViewController.swift
@@ -8,10 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct WBResponse: Decodable {
+    let results: [TVShow]
+    let count: Int
+}
 
+struct TVShow: Decodable {
+    let name: String
+    let startTime: String
+    let endTime: String
+    let channel: String
+    let rating: String
+}
+
+class ViewController: UIViewController {
+    var shows = [TVShow]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+//        shows = [TVShow]()
+        fetchResults()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +36,34 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // fetch results from url
+    func fetchResults(){
+        let urlString = "https://www.whatsbeef.net/wabz/guide.php?start=1"
+        guard let url = URL(string: urlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, err) in
+            guard let data = data else { return }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let wbResponse = try decoder.decode(WBResponse.self, from: data)
+                
+                print(wbResponse)
+                
+                self.shows.append(contentsOf: wbResponse.results)
+                
+                var i = 1;
+                for show in self.shows {
+                    print("Show \(i): \(show)");
+                    i+=1;
+                }
+                
+            } catch let jsonErr {
+                print("Error serializing json:", jsonErr)
+            }
+            
+        }.resume()
+    }
 }
 
