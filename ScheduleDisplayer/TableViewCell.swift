ab24bf2dc4ab2ad2444da3ee713c3fcb0150be32
Fiel Faustino <john.faustino@obf.ateneo.edu>
2018-07-15 17:14:20 +0800

    Created custom table view cell

---
+++ ScheduleDisplayer/TableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  TableViewCell.swift
+//  ScheduleDisplayer
+//
+//  Created by Fiel Faustino on 15/07/2018.
+//  Copyright © 2018 Fiel Faustino. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var channelImg: UIImageView!
+    @IBOutlet weak var ratingImg: UIImageView!
+    @IBOutlet weak var showName: UILabel!
+    @IBOutlet weak var showTime: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
